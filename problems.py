662305e0b113170f6ee3903df8ac3b2922f7dce0
parth8576beaift25-ctrl <parth8576.beaift25@chitkara.edu.in>
2025-08-21 09:09:05 +0530

    Add files via upload

---
+++ problems.py
@@ -0,0 +1,26 @@
+name="Parth"
+age=18
+price=22.3
+
+print(name)
+print(age)
+print(price)
+print("my name is:",name)
+
+
+
+a=int(67)
+b=int(67)
+c=a+b
+print(c)
+
+
+a=float(10.5)
+b=float(10.5)
+c=a+b
+print(c)
+
+name=True
+print(type(name))
+
+
