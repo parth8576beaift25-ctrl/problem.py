f39a6af614a2d6b08aad73bdbd7eaebf2f20f22a
parth8576beaift25-ctrl <parth8576.beaift25@chitkara.edu.in>
2025-08-22 21:04:54 +0530

    Add files via upload

---
+++ practice.py.py
@@ -0,0 +1,33 @@
+ # marks=int(input("enter marks"))
+
+#if marks>=90:
+ #  print("A")
+#elif 90>marks>=80:
+   #print("B")
+#elif 80>marks>=70:
+   #print("c")
+#elif 70>marks:
+  #   print("D")
+
+
+
+#num=9
+
+
+#rem=num % 2
+
+#if(rem == 0):
+ #  print("EVEN")
+#else:
+#   print("ODD")
+
+
+
+num=48
+
+rem=num % 7
+
+if(rem==0):
+    print("divisible by 7")
+else:
+    print("not divisible by 7")
