65675c31107bfb8eb43f75a1aa1e7cc4f6867514
parth8576beaift25-ctrl <parth8576.beaift25@chitkara.edu.in>
2025-08-25 19:04:03 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,28 @@
+#i=1
+#while i <= 100:
+ #   print(i)
+  #  i +=1
+
+#i=100
+#while i>=1:
+ #   print(i)
+  #  i-=1
+
+#i=1
+#n=3
+#while i<=10:
+    
+ #    print(n*i)
+  #   i+=1
+
+#numbers=(1,4,9,16,25,36,49,64,81,100)
+#i=0
+#while i<len(numbers):
+ #    print(numbers[i])
+  #   i+=1
+#numbers=[1,4,9,16,25,36,49,64,81,100]
+#i=0
+#while i<len(numbers):
+ #      print(numbers[i])
+  #     i+=1
+
