37cdc17506afa45d26a8dbcfbd937bd8a62a3a1e
parth8576beaift25-ctrl <parth8576.beaift25@chitkara.edu.in>
2025-08-25 19:04:28 +0530

    Add files via upload

---
+++ day3 practice.py
@@ -0,0 +1,14 @@
+#for i in range (1,1000):
+ #   print("numbers:",i)
+
+
+#fruits=["grapes","apple","mango","banana"]
+#print(fruits[0])
+#print(fruits)  #confused hai abhi
+#for fruit in fruits:
+ #   print("i like:",fruit)
+
+i=1
+while i<=5:
+    print("numbers:",i)
+    i+=1
