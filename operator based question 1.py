cc5b63e997c2fc2ce19bc909aefba384a12f356a
parth8576beaift25-ctrl <parth8576.beaift25@chitkara.edu.in>
2025-08-26 21:44:04 +0530

    Add files via upload

---
+++ operator based question 1.py
@@ -0,0 +1,12 @@
+#write a program to find the sum,difference,product and division of two numbers entered by the user.
+
+a=int(input("enter first number:"))
+b=int(input("enter second number"))
+c=a+b
+print("the sum of two number is:",c)
+d=a-b
+print("the difference of two number is:",d)
+e=a*b
+print("the product of two number is:",e)
+f=a/b
+print("the division of two number is:",f)
---
+++ operatorbasedquestion3.py
@@ -0,0 +1,8 @@
+# Write a program to check whether a given number is greater than 100 and divisible by 5 (use relational and logical operators
+
+
+a=int(input("enter a number:"))
+if a>100 and a%5==0:
+        print("it follows:")
+else:
+        print("invalid")    
