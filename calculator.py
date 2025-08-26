5717b6fd1f54b60a2ad35334e508af23ab1180ed
parth8576beaift25-ctrl <parth8576.beaift25@chitkara.edu.in>
2025-08-26 20:59:51 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,22 @@
+while True:
+     a=input("enter a value for calculation")
+     b=int(input("enter first number:"))
+     c=int(input("enter second number:"))
+     if a=="+":
+        print(b+c)
+     elif a=="-":
+         print(b-c)
+     elif a=="*":
+         print(b**c)
+     elif a=="*":
+         print(b*c)
+     elif a=="%":
+         print(b%c)
+     elif a=="/":
+         print(b/c)
+     else:
+         print("invalid")
+    
+             
+        
+    
