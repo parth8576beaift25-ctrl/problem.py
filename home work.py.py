234d573509a33b818909dac8e26a0c79a7ea939b
parth8576beaift25-ctrl <parth8576.beaift25@chitkara.edu.in>
2025-08-26 21:02:58 +0530

    Add files via upload

---
+++ home work.py.py
@@ -0,0 +1,42 @@
+#i=1     #print the first 10 natural numbers
+#while i<=10:
+      #print(i)
+     #i+=1
+
+
+#print the multiplication table of 5
+
+#i=1
+#while i<=10:
+ #   print(i*5)
+  #  i+=1
+
+#print the squares of number 1 to 10                                          #for i in range(n,m)
+#i=1
+#while i<=10:
+ #   print(i**2)
+  #  i+=1
+
+#print reverse the number from n to 1
+#n=100
+#i=100
+#while i>=1:
+ #   print(i)
+  #  i-=1
+
+# while True:
+#   num=int(input("enter a number:"))
+#   fact=1
+#   for i in range(1,1+num):
+#       fact=fact*i
+#   print("factorial of",num,"is:",fact)
+ 
+# n=int(input("enter a number:"))
+
+
+# a=0
+# b=1
+# print("fibonacci series:")
+# for i in range(n):
+#     print(a)
+#     a,b=b,b+a
