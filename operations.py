75ef82305b634033397a92928f779e464c02ed1a
parth8576beaift25-ctrl <parth8576.beaift25@chitkara.edu.in>
2025-08-26 20:58:37 +0530

    Add files via upload

---
+++ operations.py
@@ -0,0 +1,47 @@
+#airthematic operators
+
+# a=10.4
+# b=4.5
+# c=a+b  #sum
+# d=a-b  #subtraction
+# e=a*b  #multiplication
+# f=a/b  #division
+# g=a%b  #reminder
+# h=a**b #square off
+# print("the sum of the number is",c)
+# print("the subtraction of the number are ",d)
+# print("the multiplication of the number are",e)
+# print("the division of the number are", f)
+# print("the remainder of the number are",g)
+# print("the square of a**b",h)
+
+#assinment operator
+
+# a=int(input("enter first number"))
+# b=float(input("enter second number"))
+# c=a+b
+# print("the sum of the numbers are",c)
+
+# a=78.5555555555558
+# b-=4.9
+# print(a,b)
+
+
+# a-=2345
+# b+=4546564
+# print(b,a)
+
+
+
+# #logical operator(and,or,not)
+
+# a=67.9
+# b=98.8
+# print(a<b and a==b)
+# print(a>b and a==b)
+# print(a>=b and a!=b)
+# print(a<=b and a==b)
+
+
+
+
