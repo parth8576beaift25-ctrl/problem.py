9bc506dbe6cc308e88ecb6ea438ef72fc308970f
parth8576beaift25-ctrl <parth8576.beaift25@chitkara.edu.in>
2025-09-01 21:23:17 +0530

    Add files via upload

---
+++ day6.py
@@ -0,0 +1,36 @@
+#function
+# A function is a block of code that performs a specific:
+#it helps in code reusebility:
+
+# def function_name(parameters):
+#     function body:
+#     return value:
+
+
+# nums=[10,20,30]
+# print(len(nums))
+# print(max(nums))
+# print(min(nums))
+# print(sum(nums))
+
+# def square(x):
+#     return x*x
+# print(square(5))//calling function
+
+# def add(a,b):
+#     return a+b
+# res=add(int(input("enter first number:")),int(input("enter second number:")))
+# print("the sum of the numbers are:",res)
+
+
+
+# def power(a,b):
+#     return a**b
+# res=power(int(input("enter first number:")),int(input("enter second number:")))
+# print("the power of the numbers are:",res)
+
+ 
+# def sub(a,b):
+#     return a-b
+# res=sub(int(input("enter first number:")),int(input("enter second number:")))
+# print("the subtraction of the numbers are:",res)
