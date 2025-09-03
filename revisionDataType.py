83fb28fe76bb4fa249ad43af4d7c6951e3df7529
parth8576beaift25-ctrl <parth8576.beaift25@chitkara.edu.in>
2025-09-03 14:40:55 +0530

    Add files via upload

---
+++ revisionDataType.py
@@ -0,0 +1,22 @@
+# a=int(input("enter first number :"))
+# b=int(input("enter second number :"))
+# sum=a+b
+# print("the sum of the numbers are:",sum)
+
+# side=int(input("enter square side: "))
+# print("area=",side**2)
+
+
+# a=int(input("enter first number :"))
+# b=int(input("enter second number :"))
+# average=(a+b)/2
+# print("the avg of number is:",average)
+
+
+# a=int(input("enter first number :"))
+# b=int(input("enter second number :"))
+
+# if a>=b:
+#     print(True)
+# else:
+#     print(False)
