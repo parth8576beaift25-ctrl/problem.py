df47f92f707d766696092e15737501e995eb2711
parth8576beaift25-ctrl <parth8576.beaift25@chitkara.edu.in>
2025-09-03 18:15:18 +0530

    Add files via upload

---
+++ revisionStringConditionalstatement.py
@@ -0,0 +1,31 @@
+# name=str(input("enter your name:"))
+# print(len(name))
+
+
+# name=str(input("enter your name:"))
+# print(name.count("$"))
+
+# marks=int(input("Enter Marks:"))
+# if marks>=90:
+#     print("A")
+# elif 90>marks>=80:
+#     print("B")
+# elif 80>marks>=70:
+#     print("C")
+# elif 70>marks:
+#     print("D")
+# else:
+#     print("FAIL")
+
+# num=int(input("enter number:"))
+# if num%2==0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+
+# num=int(input("enter number:"))
+# if num%7==0:
+#     print("divisible by seven")
+# else:
+#     print("not divisible by 7")
