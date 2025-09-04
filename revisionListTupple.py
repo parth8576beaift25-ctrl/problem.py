c9702041d2e55037ca61a876b2a8cd4edca25d5a
parth8576beaift25-ctrl <parth8576.beaift25@chitkara.edu.in>
2025-09-04 16:58:37 +0530

    Add files via upload

---
+++ revisionListTupple.py
@@ -0,0 +1,40 @@
+# movies=[]
+# a=str(input("Enter Movie:"))
+# b=str(input("Enter Movie:"))
+# c=str(input("Enter Movie:"))
+
+# movies.append(a)
+# movies.append(b)
+# movies.append(c)
+
+# print(movies)
+
+# list1 =[1,2,3,2,1]
+# list2=[1,2,3,4,5]
+
+
+# copy_list1= list1.copy()
+# copy_list1.reverse()
+
+# if(copy_list1==list1):
+#     print("palindrome")
+# else:
+#     print("not an palindrome")
+
+
+
+# copy_list2= list2.copy()
+# copy_list2.reverse()
+
+# if(copy_list2==list2):
+#     print("palindrome")
+# else:
+#     print("not an palindrome")
+
+
+# tup=("c","d","a","a","b","b","a")
+# print(tup.count("a"))
+
+# grade=("C","D","A","A","B","B","A")
+# grade.sort()
+# print(grade)
