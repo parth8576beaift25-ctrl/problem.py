257ba69ffb5ceb14f1ea62660934a9f3c8da873d
parth8576beaift25-ctrl <parth8576.beaift25@chitkara.edu.in>
2025-09-04 10:33:19 +0530

    Add files via upload

---
+++ triange.py
@@ -0,0 +1,17 @@
+# rows=5
+# for i in range(1,rows+1):
+#     for j in range(rows-1):
+#         print(" ",end=" ")
+#     for k in range(2*i-1):
+#         print("*",end=" ")
+#     print()
+
+# rows=5
+# for i in range(1, rows + 1):
+#     # spaces
+#     for j in range(rows - i):
+#         print(" ", end=" ")
+#     # stars
+#     for k in range(2 * i - 1):
+#         print("*", end=" ")
+#     print()
