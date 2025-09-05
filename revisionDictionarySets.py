d003838b849e6ff5b903dd819c9b0aab216266cd
parth8576beaift25-ctrl <parth8576.beaift25@chitkara.edu.in>
2025-09-05 22:17:33 +0530

    Add files via upload

---
+++ revisionDictionarySets.py
@@ -0,0 +1,35 @@
+# dictionary={
+#     "cat":"a small animal",
+#     "table":["a piece of furniture","list of facts and figure"]
+# }
+
+# print(dictionary)
+
+
+# subjects={"python","java","C++","C","python","javascript","java","python","java","C++","C"}
+# print(len(subjects))
+
+# marks={}
+
+# x=int(input("enter phy:"))
+# marks.update({"phy":x})
+
+# x=int(input("enter chem:"))
+# marks.update({"chem":x})
+
+# x=int(input("enter math:"))
+# marks.update({"math":x})
+
+# print(marks)
+
+
+# set={9,"9.0"}
+# print(set)
+
+# values={
+#       ("float",9.0),
+#       ("int",9)
+    
+# }
+
+# print(values)
