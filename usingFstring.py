998f1c6f67d60c8407c299ce878f72ee2eb2cab8
parth8576beaift25-ctrl <parth8576.beaift25@chitkara.edu.in>
2025-09-10 17:27:20 +0530

    Add files via upload

---
+++ usingFstring.py
@@ -0,0 +1,8 @@
+#Write a Python program to take an integer, float, and string input from the user and print them using f-string.
+integer_input=int(input("Enter Number:"))
+float_input=float(input("Enter Number:"))
+string_input=str(input("Enter element:"))
+
+print(f"you entered the integer:{integer_input}")
+print(f"you entered the float:{float_input}")
+print(f"you entered the string:{string_input}")
