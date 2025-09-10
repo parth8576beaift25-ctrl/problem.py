132af302e67f2d1de86088531f89b901933abc43
parth8576beaift25-ctrl <parth8576.beaift25@chitkara.edu.in>
2025-09-10 10:10:30 +0530

    Add files via upload

---
+++ twoNumEqual.py
@@ -0,0 +1,10 @@
+#Write a program to check whether two numbers are equal using comparison operators.
+
+a=int(input("Enter First Number: "))
+b=int(input("Enter Second Number:"))
+if (a==b):
+    print("equal")
+elif (a!=b):
+    print("not equal")
+else:
+    print("invalid")
